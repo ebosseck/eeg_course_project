3de668c207e776d1a4f9a5d82e9f02106c4c6772
ebosseck <git-commits@mail-b.org>
2024-02-07 15:33:34 +0100

    some cleanup

--- src/tools/create1010montage.py
+++ src/tools/create1010montage.py
@@ -1,11 +1,16 @@
 from eeg_positions import get_elec_coords
 import mne
 
-# Get the electrode coordinates
-coords = get_elec_coords(system="1010", as_mne_montage=True)
+def create1010():
+    # Get the electrode coordinates
+    coords = get_elec_coords(system="1010", as_mne_montage=True)
 
-# Create the montage
-montage = mne.channels.make_standard_montage('custom_1010', pos=coords)
+    mne.channels.get_builtin_montages()
+    # Create the montage
+    #montage = mne.channels.make_standard_montage('custom_1010')
 
-# Save the montage to a file
-montage.save('custom_1010.fif')
+    # Save the montage to a file
+    #montage.save('custom_1010.fif')
+
+if __name__ == "__main__":
+    create1010()
